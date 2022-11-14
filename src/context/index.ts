726190d3cd6409c404f68f0caa197e5e5613831d
Pedro Luiz da Costa <pedroluiz51@gmail.com>
2022-11-14 10:44:14 -0300

    export HighlightableElementProviderProps as type

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,3 @@
-export {
-	default as HighlightableElementProvider,
-	HighlightableElementProviderProps,
-} from "./HighlightableElementProvider";
+export { default as HighlightableElementProvider } from "./HighlightableElementProvider";
+export type { HighlightableElementProviderProps } from "./HighlightableElementProvider";
 export { default as useHighlightableElements } from "./useHighlightableElements";
